352b641a1af0accd4d6b20179442e37dc36b09fe
jeffcv <jeff.valle@hotmail.com>
2024-01-15 06:03:16 -0300

    Creating project and git repository

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		
+		}
+	}
+}
