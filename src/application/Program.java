de80c9c546ee9d360a44b47e97c2072a6aaddcce
jeffcv <jeff.valle@hotmail.com>
2024-01-17 16:01:46 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,13 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import db.DB;
-import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
-
 		
-		}
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+		
 	}
 }
